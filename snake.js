ce023cf671cb72ce7cbbe2035452d49e4b12d52f
Milindpatel <milindpatel3959@gmail.com>
2017-07-02 01:48:52 -0400

    function createSnake, createFruit and some supportive function

--- snake.js
+++ snake.js
@@ -39,10 +39,10 @@ function createMap(){
     for(var y = 0; y < height; y++){
         document.write("<tr>");
         for(var x = 0; x < width ;x++){
-            if(x == 0 || x == width-1 || y==0 ||y == height-1){
-                document.write("<td class='wall' id =' "+ x + "-" + y +"' ></td>");
+            if(x == 0 || x == width-1 || y == 0 || y == height-1 ){
+                document.write("<td class='wall' id ='" + x + "-" + y + "' ></td>");
             }else{
-                document.write("<td class='blank' id =' "+ x + "-" + y +"' ></td>");
+                document.write("<td class='blank' id ='" + x + "-" + y + "' ></td>");
             }
         }
         document.write("</tr>");
@@ -51,4 +51,35 @@ function createMap(){
     document.write("</table>");
 }
 
+function createSnake(){
+    set(snakeX,snakeY,"snake");
+}
+function get(x,y){ 
+    return document.getElementById(x+"-"+y); 
+} 
+function set(x,y,value){
+   if(x != null && y != null) 
+        get(x,y).setAttribute("class", value); 
+}
+
+function rand(min,max){
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+function getType(x,y){
+    return get(x,y).getAttribute("class");
+}
+
+function createFruit(){
+    var found = false;
+    while(!found && (length < (width-2)*(height-2)+1)){
+            var fruitX = rand(1,width-1);
+            var fruitY = rand(1,height-1);
+            if(getType(fruitX,fruitY) == "blank")
+                found = true;
+    }
+    set(fruitX, fruitY, "fruit");
+    fx = fruitX;
+    fy = fruitY;
+}
 run();
